08176e73fbb2de9d480f554940d0682f1a045ed8
Dondon Herrera <dondonherrera02@gmail.com>
2020-03-22 15:44:04 +0800

    Revert "Revert "Added association of entities between ng and ef""
    This reverts commit 8bd19b3672f690832eefed691b3dae768a009b10.

--- src/app/Questions/Questions.component.ts
+++ src/app/Questions/Questions.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../_service/api.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-Questions',
@@ -9,12 +10,15 @@ import { ApiService } from '../_service/api.service';
 export class QuestionsComponent implements OnInit {
   question :  any = {};
   questionList
+  quizId
 
-  constructor(public api: ApiService) { }
+  constructor(public api: ApiService, private route : ActivatedRoute ) { }
 
   ngOnInit() {
-    this.api.getQuestions().subscribe(res => {
-      this.questionList = res
+    var quizId = this.route.snapshot.paramMap.get('quizId');
+
+    this.api.getQuestions(quizId).subscribe(res => {
+      this.questionList = res;this.questionList = Array.of(this.questionList); 
     });
   }
 
--- src/app/_service/api.service.ts
+++ src/app/_service/api.service.ts
@@ -10,6 +10,8 @@ export class ApiService {
   private selectedQuestion = new Subject<any>(); //will hold reference to the question that we clicked on and selected.
   questionSelected = this.selectedQuestion.asObservable();  
 
+  private selectedQuiz = new Subject<any>();
+  quizSelected = this.selectedQuiz.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -23,12 +25,31 @@ export class ApiService {
     })
   }
 
-  getQuestions() {
-    return this.http.get('https://localhost:5001/api/questions')
+  getQuestions(quizId) {
+    return this.http.get('https://localhost:5001/api/questions/' + quizId)
   }
 
   //function that will actually set question selected
   selectQuestion(question) {
     this.selectedQuestion.next(question);
   }
+
+  postQuiz(quiz) {
+    this.http.post('https://localhost:5001/api/quizzes', quiz).subscribe(res => {
+    })
+  }
+
+  getQuizzes() {
+    return this.http.get('https://localhost:5001/api/quizzes')
+  }
+
+  putQuiz(quiz) {
+    this.http.put('https://localhost:5001/api/quizzes/' + quiz.id, quiz).subscribe(res => {
+    })
+  }
+
+  selectQuiz(quiz) {
+    this.selectedQuiz.next(quiz);
+  }
+
 }
