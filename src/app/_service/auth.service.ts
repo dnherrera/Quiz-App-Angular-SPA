2ca7b1a711d14f21eae0989c24a3ef1415e4d571
Dondon Herrera <dondonherrera02@gmail.com>
2020-04-10 12:32:22 +0800

    Added Authentication features

---
+++ src/app/_service/auth.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError} from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+baseUrl = 'https://localhost:5001/api/auth/';
+userToken: any;
+
+options = {
+  headers: new HttpHeaders().append('content-type', 'application/json')
+}
+
+constructor(private http: HttpClient) { }
+
+register(model: any) {
+  return this.http.post(this.baseUrl + 'register', model, this.options).pipe(catchError(this.handleError));
+}
+
+login(model: any) {
+  return this.http
+  .post(this.baseUrl + 'login', model, this.options)
+  .pipe(map((response:any) => {
+    const user  = response
+    if(user) {
+      localStorage.setItem('token', user.tokenString);
+      this.userToken = user.tokenString;
+    }
+  }))
+  .pipe(catchError(this.handleError));
+}
+
+loggedIn() {
+  const token = localStorage.getItem('token');
+  return !!token;
+}
+
+private handleError(error: any) {
+  const applicationError = error.headers.get('Application-Error');
+  if (applicationError) {
+    return Observable.throw(applicationError);
+  }
+  const serverError = error;
+  let modelStateErrors = '';
+  if (serverError) {
+    for (const key in serverError) {
+      if (serverError[key]) {
+        modelStateErrors += serverError[key] + '\n';
+      }
+    }
+  }
+  return throwError(
+    modelStateErrors || 'Server error'
+  );
+}
+
+
+}
+
+
